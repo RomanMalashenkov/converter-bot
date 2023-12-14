3ff863be242d335da6cf96eae5818396d4cf75a5
RomanMalashenkov <malashenckov.roma@yandex.ru>
2023-12-14 18:28:40 +0300

    3

---
+++ api/api.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"fmt"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func getFileURL(bot *tele.Bot, fileID string) (string, error) {
+	fileInfo, err := bot.FileByID(fileID)
+	if err != nil {
+		return "", err
+	}
+
+	// Получаем информацию о файле
+	filePath := fileInfo.FilePath
+
+	// Формируем URL для скачивания файла
+	// Для получения прямой ссылки используем URL Telegram Bot API
+	fileURL := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", bot.Token, filePath)
+
+	return fileURL, nil
+}
--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -16,22 +16,6 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
-func getFileURL(bot *tele.Bot, fileID string) (string, error) {
-	fileInfo, err := bot.FileByID(fileID)
-	if err != nil {
-		return "", err
-	}
-
-	// Получаем информацию о файле
-	filePath := fileInfo.FilePath
-
-	// Формируем URL для скачивания файла
-	// Для получения прямой ссылки используем URL Telegram Bot API
-	fileURL := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", bot.Token, filePath)
-
-	return fileURL, nil
-}
-
 func StartBot() {
 	botConf, confErr := config.GetConfig()
 	if confErr != nil {
@@ -84,7 +68,7 @@ func StartBot() {
 		}
 
 		// Получаем URL файла по его FileID
-		fileURL, err := getFileURL(b, doc.FileID)
+		fileURL, err := api.getFileURL(b, doc.FileID)
 		if err != nil {
 			log.Printf("Failed to get file URL: %s", err)
 			return err
